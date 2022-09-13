b927f602c30ba0f7092ee39f146b571085560b97
rusokverse <rusokverse@MACBOOKPRO>
2022-09-13 18:56:04 -0400

    agregando using

--- EntityFramework1/Form1.cs
+++ EntityFramework1/Form1.cs
@@ -25,9 +25,13 @@ namespace EntityFramework1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string cadenaDeConexion = @"Server=.\SQLDEVELOPERCQ;DataBase=Supermercado;User=Pepe;Password=123456";
-            SqlConnection con = new SqlConnection(cadenaDeConexion);
-            con.Open();
+            string cadenaDeConexion = @"Server=192.168.1.253,1433;DataBase=Supermercado;User=Pepe;Password=123456";
+            using (SqlConnection con = new SqlConnection(cadenaDeConexion)) { 
+                con.Open();
+                label1.Text = "Estado: Conectado";
+
+            }
+                
         }
     }
 }
